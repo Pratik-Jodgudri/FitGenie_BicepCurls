bfaddff76a7f1fc2a05dbba44c835d61b1080a26
Pratik Jodgudri <87020427+Pratik-Jodgudri@users.noreply.github.com>
2024-06-10 13:57:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,39 +18,39 @@ class VideoProcessor(VideoProcessorBase):
 
         maxh, maxw, _ = img.shape
 
-        # Process frame with pose estimation
-        img = self.detector.findPose(img, False)
-        lmList = self.detector.findPosition(img, False)
+        # # Process frame with pose estimation
+        # img = self.detector.findPose(img, False)
+        # lmList = self.detector.findPosition(img, False)
 
-        # Check if landmarks are detected
-        if len(lmList) != 0:
-            # Calculate angle
-            angle = self.detector.findAngle(img, 12, 14, 16)
+        # # Check if landmarks are detected
+        # if len(lmList) != 0:
+        #     # Calculate angle
+        #     angle = self.detector.findAngle(img, 12, 14, 16)
 
-            per = np.interp(angle, (55, 145), (100, 0))
-            bar = np.interp(angle, (55, 145), (400, 50))
+        #     per = np.interp(angle, (55, 145), (100, 0))
+        #     bar = np.interp(angle, (55, 145), (400, 50))
 
-            # Check for the dumbbell curls
-            color = (255, 0, 255)
-            if per == 100:
-                color = (0, 255, 0)
-                if self.direction == 0:
-                    self.count += 0.5
-                    self.direction = 1
-            if per == 0:
-                color = (0, 255, 0)
-                if self.direction == 1:
-                    self.count += 0.5
-                    self.direction = 0
+        #     # Check for the dumbbell curls
+        #     color = (255, 0, 255)
+        #     if per == 100:
+        #         color = (0, 255, 0)
+        #         if self.direction == 0:
+        #             self.count += 0.5
+        #             self.direction = 1
+        #     if per == 0:
+        #         color = (0, 255, 0)
+        #         if self.direction == 1:
+        #             self.count += 0.5
+        #             self.direction = 0
 
-            # Draw Bar
-            cv2.rectangle(img, (maxw-100, maxh-400), (maxw - 50, maxh - 50), color, 3)
-            cv2.rectangle(img, (maxw - 100, maxh - int(bar)), (maxw - 50, maxh - 50), color, cv2.FILLED)
-            cv2.putText(img, f'{int(per)} %', (maxw - 105, maxh - 430), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+        #     # Draw Bar
+        #     cv2.rectangle(img, (maxw-100, maxh-400), (maxw - 50, maxh - 50), color, 3)
+        #     cv2.rectangle(img, (maxw - 100, maxh - int(bar)), (maxw - 50, maxh - 50), color, cv2.FILLED)
+        #     cv2.putText(img, f'{int(per)} %', (maxw - 105, maxh - 430), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
 
-            # Draw Curl Count
-            cv2.rectangle(img, (0, 0), (150, 100), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, str(int(self.count)), (40, 70), cv2.FONT_HERSHEY_PLAIN, 4, (255, 0, 0), 4)
+        #     # Draw Curl Count
+        #     cv2.rectangle(img, (0, 0), (150, 100), (0, 255, 0), cv2.FILLED)
+        #     cv2.putText(img, str(int(self.count)), (40, 70), cv2.FONT_HERSHEY_PLAIN, 4, (255, 0, 0), 4)
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
